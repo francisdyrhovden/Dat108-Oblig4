7f1ce8c69bf97e6343604196aad7297ac48b44a8
Francis Soliman <Francis@Franciss-MacBook-Pro.local>
2019-11-17 21:44:54 +0100

    Finished :boom:

---
+++ src/no/hvl/dat108/graphs/DepthFirstSearch.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package no.hvl.dat108.graphs;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+
+/**
+ * @author tdoy
+ *
+ */
+public class DepthFirstSearch {
+
+	private String source;
+	private Graph graph;
+	
+	public DepthFirstSearch(Graph graph, String source) {
+		
+		this.graph = graph;
+		this.source = source;
+	}
+	
+	public Set<String> doDepthFirstSearch(){
+		
+		Set<String> visited = new LinkedHashSet<String>();
+		Stack<String> stack = new Stack<String>();
+		
+		//push the source in the stack
+		stack.push(source);
+		
+		// implement me
+		while (!stack.isEmpty()) {
+			source = stack.pop();
+			if (!visited.contains(source)) {
+				visited.add(source);
+				for (Node node : graph.getAdjacentNodes(source)) {
+					stack.push(node.getName());
+				}
+			}
+		}
+				
+		return visited;
+	}
+	
+	public Set<String> doDepthFirstSearchRecursive(){
+		
+		Set<String> visited = new LinkedHashSet<String>();
+		List<Node> nodes = new ArrayList<Node>(graph.getAdjNodes().keySet());
+		
+		// use nodes
+		Node src = nodes.get(nodes.indexOf(new Node(source)));
+
+		// call dfsRecursive method here
+		dfsRecursive(nodes, src, visited);
+		
+				
+		return visited;
+	}
+	
+	public void dfsRecursive(List<Node> nodes, Node src, Set<String> visited) {
+		
+		// implement me
+		visited.add(src.getName());
+		for (Node node : graph.getAdjacentNodes(src.getName())) {
+			if (!visited.contains(node.getName())) {
+				dfsRecursive(nodes, node, visited);
+			}
+		}
+		
+	}
+	
+}
---
+++ src/no/hvl/dat108/graphs/VertexCover.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package no.hvl.dat108.graphs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author tdoy
+ *
+ */
+public class VertexCover {
+
+	/**
+	 * 
+	 */
+
+	private Graph graph;
+
+	public VertexCover(Graph graph) {
+
+		this.graph = graph;
+
+	}
+
+	public List<String> approxVertexCover() {
+
+		List<String> approxmvc = new ArrayList<String>(); // set to hold our min vertex cover
+
+		// collect the edges of the graph
+		Queue<Edge> queue = new LinkedList<Edge>(graph.getEdges()); // set of all vertices
+
+		// implement me
+		while (!queue.isEmpty()) {
+			approxmvc.add(queue.peek().getSource());
+			approxmvc.add(queue.peek().getDestination());
+			List<Edge> remEdges = new ArrayList<Edge>(queue);
+			for (Edge e : remEdges) {
+				if (approxmvc.contains(e.getSource()) || approxmvc.contains(e.getDestination())) {
+					queue.remove(e);
+				}
+			}
+		}
+		return approxmvc;
+	}
+
+}
